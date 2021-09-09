d31544976eb44753d0b7ed95594317d45b0e63ba
BlankZhu <z707310837@gmail.com>
2021-09-09 09:03:28 +0800

    update: use boost-json header-only

--- src/BlankHttpConfig.h
+++ src/BlankHttpConfig.h
@@ -11,9 +11,56 @@ namespace blank
     struct BlankHttpConfig
     {
     public:
-        void calibrate();
-        std::string to_readable_string() const;
-        std::string to_json_string() const;
+        void calibrate()
+        {
+            if (address.empty())
+            {
+                address = "0.0.0.0";
+            }
+            if (port == 0)
+            {
+                port = 8077;
+            }
+            if (timeout <= 0)
+            {
+                timeout = 5;
+            }
+            if (threads == 0)
+            {
+                threads = static_cast<int>(std::thread::hardware_concurrency());
+            }
+            if (log_level > 5)
+            {
+                log_level = 5;
+            }
+            if (log_level < 0)
+            {
+                log_level = 0;
+            }
+        }
+
+        std::string to_readable_string() const
+        {
+            std::stringstream ss;
+            ss << "serving at: [" << address << ":" << port << "], "
+               << "with HTTP timeout: [" << timeout << "] in second, "
+               << "running in thread number: [" << threads << "], "
+               << "on log level: [" << log_level << "]";
+            return ss.str();
+        }
+
+        std::string to_json_string() const
+        {
+            namespace json = boost::json;
+            json::object obj{};
+            obj["timeout"] = timeout;
+            obj["address"] = address;
+            obj["port"] = port;
+            obj["threads"] = threads;
+            obj["log_level"] = log_level;
+
+            return json::serialize(obj);
+        }
 
     public:
         int log_level;
