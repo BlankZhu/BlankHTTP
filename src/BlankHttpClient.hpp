c65db3cef1df3ccb041f81130c4af4d6efcade0d
BlankZhu <BlankZhu47@gmail.com>
2021-09-10 12:03:01 +0800

    update: split client codes

--- src/BlankHttpClient.hpp
+++ src/BlankHttpClient.hpp
@@ -22,115 +22,20 @@ namespace blank
                         const std::chrono::seconds &timeout, net::any_io_executor executor)
             : host_(host), port_(port), timeout_(timeout), is_connected_(false),
               resolver_(executor), stream_(executor) {}
-
         BlankHttpClient(BlankHttpClient &&) = default;
+        ~BlankHttpClient();
 
-        ~BlankHttpClient()
-        {
-            if (is_connected_)
-            {
-                beast::error_code ec;
-                Shutdown(ec);
-            }
-        }
-
-        void Connect(net::yield_context &yield, beast::error_code &ec)
-        {
-            if (is_connected_)
-            {
-                return;
-            }
-
-            stream_.expires_after(timeout_);
-            auto const results = resolver_.async_resolve(host_, port_, yield[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            stream_.async_connect(results, yield[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            is_connected_ = true;
-        }
-
-        void Shutdown(beast::error_code &ec)
-        {
-            if (is_connected_)
-            {
-                stream_.expires_after(timeout_);
-                stream_.socket().shutdown(tcp::socket::shutdown_both, ec);
-                is_connected_ = false;
-            }
-        }
-
+    public:
+        void Connect(net::yield_context &yield, beast::error_code &ec);
+        void Shutdown(beast::error_code &ec);
         template <class Body, class Fields>
         void DoRequest(const http::message<true, Body, Fields> &request,
                        http::message<false, Body, Fields> &response,
-                       net::yield_context &yield, beast::error_code &ec)
-        {
-            if (!is_connected_)
-            {
-                Connect(yield, ec);
-                if (ec)
-                {
-                    return;
-                }
-            }
-
-            stream_.expires_after(timeout_);
-            http::async_write(stream_, request, yield[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            beast::flat_buffer buffer;
-            http::async_read(stream_, buffer, response, yield[ec]);
-            if (ec)
-            {
-                return;
-            }
-        }
-
+                       net::yield_context &yield, beast::error_code &ec);
         template <class Body, class Fields>
         void Download(const http::message<true, Body, Fields> &request,
                       const std::string &save_path,
-                      net::yield_context &yield, beast::error_code &ec)
-        {
-            if (!is_connected_)
-            {
-                Connect(yield, ec);
-                if (ec)
-                {
-                    return;
-                }
-            }
-
-            http::response<http::file_body> response;
-            response.body().open(save_path.c_str(), beast::file_mode::write, ec);
-            if (ec)
-            {
-                return;
-            }
-
-            stream_.expires_after(timeout_);
-            http::async_write(stream_, request, yield[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            beast::flat_buffer buffer;
-            http::async_read(stream_, buffer, response, yield[ec]);
-            if (ec)
-            {
-                return;
-            }
-        }
+                      net::yield_context &yield, beast::error_code &ec);
 
     private:
         std::string host_;
--- src/BlankHttpsClient.hpp
+++ src/BlankHttpsClient.hpp
@@ -37,135 +37,20 @@ namespace blank
               yield_(yield)
         {
         }
-
         BlankHttpsClient(BlankHttpsClient &&) = default;
+        ~BlankHttpsClient();
 
-        ~BlankHttpsClient()
-        {
-            if (is_connected_)
-            {
-                beast::error_code ec;
-                Shutdown(ec);
-            }
-        }
-
-        void Connect(beast::error_code &ec)
-        {
-            if (is_connected_)
-            {
-                return;
-            }
-
-            // setup SNI Hostname
-            if (!SSL_set_tlsext_host_name(stream_.native_handle(), host_.c_str()))
-            {
-                ec.assign(static_cast<int>(::ERR_get_error()), net::error::get_ssl_category());
-                return;
-            }
-
-            auto const endpoints = resolver_.async_resolve(host_, port_, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            // set timeout
-            beast::get_lowest_layer(stream_).expires_after(timeout_);
-            beast::get_lowest_layer(stream_).async_connect(endpoints, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            // perform ssl handshake
-            stream_.async_handshake(ssl::stream_base::client, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            is_connected_ = true;
-        }
-
-        void Shutdown(beast::error_code &ec)
-        {
-            if (is_connected_)
-            {
-                beast::get_lowest_layer(stream_).expires_after(timeout_);
-                stream_.async_shutdown(yield_[ec]);
-                if (ec == net::error::eof)
-                {
-                    ec = {};
-                }
-
-                is_connected_ = false;
-            }
-        }
-
+    public:
+        void Connect(beast::error_code &ec);
+        void Shutdown(beast::error_code &ec);
         template <class Body, class Fields>
         void DoRequest(const http::message<true, Body, Fields> &request,
                        http::message<false, Body, Fields> &response,
-                       beast::error_code &ec)
-        {
-            if (!is_connected_)
-            {
-                Connect(ec);
-                if (ec)
-                {
-                    return;
-                }
-            }
-
-            beast::get_lowest_layer(stream_).expires_after(timeout_);
-            http::async_write(stream_, request, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            beast::flat_buffer buffer;
-            http::async_read(stream_, buffer, response, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-        }
-
+                       beast::error_code &ec);
         template <class Body, class Fields>
         void Download(const http::message<true, Body, Fields> &request,
                       const std::string &save_path,
-                      beast::error_code &ec)
-        {
-            if (!is_connected_)
-            {
-                Connect(ec);
-                if (ec)
-                {
-                    return;
-                }
-            }
-
-            http::response<http::file_body> response;
-            response.body().open(save_path.c_str(), beast::file_mode::write, ec);
-            if (ec)
-            {
-                return;
-            }
-
-            beast::get_lowest_layer(stream_).expires_after(timeout_);
-            http::async_write(stream_, request, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-
-            beast::flat_buffer buffer;
-            http::async_read(stream_, buffer, response, yield_[ec]);
-            if (ec)
-            {
-                return;
-            }
-        }
+                      beast::error_code &ec);
 
     private:
         std::string host_;
