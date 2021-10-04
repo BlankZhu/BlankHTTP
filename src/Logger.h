0db8d9dd51bab32db5ecbc748987d89005e23324
BlankZhu <BlankZhu47@gmail.com>
2021-10-04 15:56:55 +0800

    fix severity not working error

--- src/Logger.h
+++ src/Logger.h
@@ -31,17 +31,18 @@ class Logger {
     ~Logger();
 
    public:
-    void init(int log_level = 1);
-    void init(int log_level, const std::string &log_filename);
-    int trace(fmt formatted);
-    int debug(fmt formatted);
-    int info(fmt formatted);
-    int warning(fmt formatted);
-    int error(fmt formatted);
-    int fatal(fmt formatted);
+    void init(int log_level = 1, const std::string &log_filename = "");
+    int trace(const fmt &formatted);
+    int debug(const fmt &formatted);
+    int info(const fmt &formatted);
+    int warning(const fmt &formatted);
+    int error(const fmt &formatted);
+    int fatal(const fmt &formatted);
 
    private:
-    int do_logging(fmt &formatted, trivial::severity_level level);
+    void set_os_log();
+    void set_file_log(const std::string &log_filename);
+    int do_logging(const fmt &formatted, trivial::severity_level level);
     trivial::severity_level parse_log_level(int log_level);
 
    private:
