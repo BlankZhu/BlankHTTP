35904f68b34c52c9eccbb9984889f894b7ed2d83
BlankZhu <BlankZhu47@gmail.com>
2021-11-18 14:21:37 +0800

    update: merge client

--- src/Client.hpp
+++ src/Client.hpp
@@ -1,29 +1,41 @@
 #pragma once
 
 #include <chrono>
+#include <cstdint>
+#include <memory>
 #include <string>
 
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
+#include <boost/beast/ssl.hpp>
 
 namespace blank {
 namespace beast = boost::beast;
 namespace http = boost::beast::http;
 namespace net = boost::asio;
+namespace ssl = boost::asio::ssl;
 using tcp = boost::asio::ip::tcp;
 
 class Client {
  public:
-  Client(const std::string &host, const std::string &port,
-         const std::chrono::seconds &timeout, net::any_io_executor executor)
+  Client(const std::string &host, std::uint_least16_t port,
+         const std::chrono::seconds &timeout,
+         std::shared_ptr<ssl::context> ssl_context,
+         net::any_io_executor executor)
       : host_(host),
-        port_(port),
+        port_(std::to_string(port)),
         timeout_(timeout),
-        is_connected_(false),
+        ssl_context_(ssl_context),
         resolver_(executor),
-        stream_(executor) {}
+        is_connected_(false) {
+    if (ssl_context_ != nullptr) {
+      ssl_stream_.emplace(executor, *ssl_context_);
+    } else {
+      tcp_stream_.emplace(executor);
+    }
+  }
   Client(Client &&) = default;
   ~Client() {
     if (is_connected_) {
@@ -33,93 +45,141 @@ class Client {
   }
 
  public:
-  void Connect(net::yield_context &yield, beast::error_code &ec) {
+  void Connect(net::yield_context yield, beast::error_code &ec) {
     if (is_connected_) {
       return;
     }
 
-    stream_.expires_after(timeout_);
+    if (tcp_stream_.has_value()) {
+      connect(yield, ec);
+    }
+    if (ssl_stream_.has_value()) {
+      connect_ssl(yield, ec);
+    }
+    is_connected_ = true;
+  }
+
+  void Shutdown(beast::error_code &ec) {
+    if (!is_connected_) {
+      return;
+    }
+
+    if (tcp_stream_.has_value()) {
+      shutdown(ec);
+    }
+    if (ssl_stream_.has_value()) {
+      shutdown_ssl(ec);
+    }
+    is_connected_ = false;
+  }
+
+  template <class RequestBody, class ResponseBody>
+  void Do(const http::request<RequestBody> &req,
+          http::response<ResponseBody> &resp, net::yield_context yield,
+          beast::error_code &ec) {
+    if (!is_connected_) {
+      Connect(yield, ec);
+      if (ec) {
+        return;
+      }
+    }
+
+    if (tcp_stream_.has_value()) {
+      return send_and_recv(req, resp, yield, ec);
+    }
+    if (ssl_stream_.has_value()) {
+      return send_and_recv_ssl(req, resp, yield, ec);
+    }
+  }
+
+ private:
+  void connect(net::yield_context yield, beast::error_code &ec) {
     auto const results = resolver_.async_resolve(host_, port_, yield[ec]);
     if (ec) {
       return;
     }
 
-    stream_.async_connect(results, yield[ec]);
+    tcp_stream_->expires_after(timeout_);
+    tcp_stream_->async_connect(results, yield[ec]);
+    if (ec) {
+      return;
+    }
+  }
+  void connect_ssl(net::yield_context yield, beast::error_code &ec) {
+    // setup SNI hostname
+    if (!SSL_set_tlsext_host_name(ssl_stream_->native_handle(),
+                                  host_.c_str())) {
+      ec.assign(static_cast<int>(::ERR_get_error()),
+                net::error::get_ssl_category());
+      return;
+    }
+
+    auto const results = resolver_.async_resolve(host_, port_, yield[ec]);
     if (ec) {
       return;
     }
 
-    is_connected_ = true;
-  }
-
-  void Shutdown(beast::error_code &ec) {
-    if (is_connected_) {
-      stream_.expires_after(timeout_);
-      stream_.socket().shutdown(tcp::socket::shutdown_both, ec);
-      is_connected_ = false;
-    }
-  }
-
-  template <class Body, class Fields>
-  void DoRequest(const http::message<true, Body, Fields> &request,
-                 http::message<false, Body, Fields> &response,
-                 net::yield_context &yield, beast::error_code &ec) {
-    if (!is_connected_) {
-      Connect(yield, ec);
-      if (ec) {
-        return;
-      }
-    }
-
-    stream_.expires_after(timeout_);
-    http::async_write(stream_, request, yield[ec]);
+    beast::get_lowest_layer(*ssl_stream_).expires_after(timeout_);
+    beast::get_lowest_layer(*ssl_stream_).async_connect(results, yield[ec]);
     if (ec) {
       return;
     }
 
+    ssl_stream_->async_handshake(ssl::stream_base::client, yield[ec]);
+    if (ec) {
+      return;
+    }
+  }
+
+  void shutdown(beast::error_code &ec) {
+    tcp_stream_->expires_after(timeout_);
+    tcp_stream_->socket().shutdown(tcp::socket::shutdown_both, ec);
+    return;
+  }
+  void shutdown_ssl(beast::error_code &ec) {
+    beast::get_lowest_layer(*ssl_stream_).expires_after(timeout_);
+    beast::get_lowest_layer(*ssl_stream_)
+        .socket()
+        .shutdown(tcp::socket::shutdown_both, ec);
+    return;
+  }
+
+  template <class RequestBody, class ResponseBody>
+  void send_and_recv(const http::request<RequestBody> &req,
+                     http::response<ResponseBody> &resp,
+                     net::yield_context yield, beast::error_code &ec) {
+    tcp_stream_->expires_after(timeout_);
+    http::async_write(*tcp_stream_, req, yield[ec]);
+    if (ec) {
+      return;
+    }
     beast::flat_buffer buffer;
-    http::async_read(stream_, buffer, response, yield[ec]);
-    if (ec) {
-      return;
-    }
+    http::async_read(*tcp_stream_, buffer, resp, yield[ec]);
+    return;
   }
-
-  template <class Body, class Fields>
-  void Download(const http::message<true, Body, Fields> &request,
-                const std::string &save_path, net::yield_context &yield,
-                beast::error_code &ec) {
-    if (!is_connected_) {
-      Connect(yield, ec);
-      if (ec) {
-        return;
-      }
-    }
-
-    http::response<http::file_body> response;
-    response.body().open(save_path.c_str(), beast::file_mode::write, ec);
+  template <class RequestBody, class ResponseBody>
+  void send_and_recv_ssl(const http::request<RequestBody> &req,
+                         http::response<ResponseBody> &resp,
+                         net::yield_context yield, beast::error_code &ec) {
+    beast::get_lowest_layer(*ssl_stream_).expires_after(timeout_);
+    http::async_write(*ssl_stream_, req, yield[ec]);
     if (ec) {
       return;
     }
-
-    stream_.expires_after(timeout_);
-    http::async_write(stream_, request, yield[ec]);
-    if (ec) {
-      return;
-    }
-
     beast::flat_buffer buffer;
-    http::async_read(stream_, buffer, response, yield[ec]);
-    if (ec) {
-      return;
-    }
+    http::async_read(*ssl_stream_, buffer, resp, yield[ec]);
+    return;
   }
 
  private:
-  std::string host_;
-  std::string port_;
-  std::chrono::seconds timeout_;
-  bool is_connected_;
+  const std::string host_;
+  const std::string port_;
+  const std::chrono::seconds timeout_;
   tcp::resolver resolver_;
-  beast::tcp_stream stream_;
+  bool is_connected_;
+  std::shared_ptr<ssl::context> ssl_context_;
+  boost::optional<beast::tcp_stream> tcp_stream_;
+  boost::optional<beast::ssl_stream<beast::tcp_stream>> ssl_stream_;
 };
+
 }  // namespace blank
